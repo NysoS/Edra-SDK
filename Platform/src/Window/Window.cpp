3fd684b382b5b9de0433df8d5685e1f5dcbefba4
Kristofer <k.ledoux.dev@gmail.com>
2026-01-21 22:28:04 +0100

    fix platformOS ABI context destruction

--- Platform/src/Window/Window.cpp
+++ Platform/src/Window/Window.cpp
@@ -2,23 +2,19 @@
 
 #include <iostream>
 
-static std::mutex gContextMutex;
-static std::shared_ptr<EdraSDK::Platform::WindowABIContext> gWinABIContext = nullptr;
+static std::shared_ptr<EdraSDK::Platform::WindowABIContext> getGlobalWindowABIContext()
+{
+	static auto sInstance = std::make_shared<EdraSDK::Platform::WindowABIContext>();
+	return sInstance;
+}
 
 EdraSDK::Platform::Window::Window(const WindowDesc& pWinDesc)
 	: mHandle(nullptr)
 {
-	if (!gWinABIContext)
-	{
-		std::lock_guard<std::mutex> lLock(gContextMutex);
-		if (!gWinABIContext)
-		{
-			gWinABIContext = std::make_shared<EdraSDK::Platform::WindowABIContext>();
-			std::cout << "[SDK] New ABI Context created" << std::endl;
-		}
-	}
-
-	mContext = gWinABIContext;
+	mContext = ::getGlobalWindowABIContext();
+	if (!mContext)
+		throw std::runtime_error("[SDK] No Window ABI Context finded");
+	
 	std::cout << "[SDK] Reusing existing ABI Context" << std::endl;
 	mHandle = mContext->create(pWinDesc);
 }
